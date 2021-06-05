b033328e607f07f71f8428593a249247fd663afe
“xzpCSCKFWMS <l>
2021-06-06 00:51:45 +0800

    feat(首页头部下拉菜单): 组件通信 结合 v-model的实现 与 computed 的set 方法 再 结合vuex 去修改state

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,46 @@
 <template>
-  <div>home</div>
+  <div>
+    <!-- <home-header :value="value" @input="change"></home-header> -->
+    <home-header v-model="currentCategory"></home-header>
+    {{currentCategory}}
+  </div>
 </template>
 
 <script>
-export default {
-
+import * as Types from "@/store/actions-types";
+import HomeHeader from "./home-header.vue";
+import { createNamespacedHelpers } from "vuex";
+let {mapState:mapHomeState,mapMutations, mapActions} = createNamespacedHelpers('home')
+export default { 
+  components:{
+    HomeHeader
+  },
+  computed:{
+    ...mapHomeState(['category','slides']),
+    currentCategory:{
+      get(){
+        return this.category
+      },
+      set(value){
+        this[Types.SET_CATEGORY](value)
+      }
+    }
+  }, 
+  data(){
+    // 全部课程 -1 node课程 0 react课程1 vue课程2
+    return {
+      value:-1
+    }
+  },
+  methods:{
+    ...mapMutations([Types.SET_CATEGORY]),
+    ...mapActions([Types.SET_SLIDES])
+  },
+  mounted(){
+    if (!this.slides.length) {
+      this[Types.SET_SLIDES]()
+    }
+  }
 }
 </script>
 
